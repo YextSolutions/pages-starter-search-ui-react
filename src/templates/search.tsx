a16c48d146a4371f797e902e93551bd68b9bf4cf
Aaron Pavlick <apavlick@yext.com>
2022-11-09 15:44:50 -0800

    module 1

--- src/templates/search.tsx
+++ src/templates/search.tsx
@@ -8,6 +8,20 @@ import {
   TemplateProps,
 } from "@yext/pages";
 import "../index.css";
+import {
+  SearchHeadlessProvider,
+  provideHeadless,
+  HeadlessConfig,
+  SandboxEndpoints,
+} from "@yext/search-headless-react";
+import {
+  SearchBar,
+  StandardCard,
+  VerticalResults,
+  SpellCheck,
+  ResultsCount,
+  Pagination,
+} from "@yext/search-ui-react";
 
 export const getPath: GetPath<TemplateProps> = () => {
   return "/search";
@@ -23,8 +37,33 @@ export const getHeadConfig: GetHeadConfig<
   };
 };
 
+const headlessConfig: HeadlessConfig = {
+  apiKey: "9f82d5b5eb50a85eb755a14edc9bf780",
+  experienceKey: "turtlehead",
+  locale: "en",
+  verticalKey: "faqs",
+  endpoints: SandboxEndpoints,
+};
+
+const searcher = provideHeadless(headlessConfig);
+
 const Search: Template<TemplateRenderProps> = () => {
-  return <>Your Code Here!</>;
+  return (
+    <SearchHeadlessProvider searcher={searcher}>
+      <div className="px-4 py-8">
+        <div className="mx-auto flex max-w-5xl flex-col">
+          <SearchBar />
+          <SpellCheck />
+          <ResultsCount />
+          <VerticalResults
+            CardComponent={StandardCard}
+            displayAllOnNoResults={false}
+          />
+        </div>
+        <Pagination />
+      </div>
+    </SearchHeadlessProvider>
+  );
 };
 
 export default Search;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,42 @@
+// The following import is the path to where the component source code lives
+const { ComponentsContentPath } = require("@yext/search-ui-react");
+
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./lib/**/*.{js,jsx}",
+    // tailwind is applied to the components by adding the component path here
+    ComponentsContentPath,
+  ],
   theme: {
-    extend: {},
+    // the default theme is extended with custom styling used by the components
+    extend: {
+      colors: {
+        primary: "var(--primary-color, #2563eb)",
+        "primary-light": "var(--primary-color-light, #dbeafe)",
+        "primary-dark": "var(--primary-color-dark, #1e40af)",
+        neutral: "var(--neutral-color, #4b5563)",
+        "neutral-light": "var(--neutral-color-light, #9ca3af)",
+        "neutral-dark": "var(--neutral-color-dark, #1f2937)",
+      },
+      borderRadius: {
+        cta: "var(--cta-border-radius, 1rem)",
+      },
+      keyframes: {
+        rotate: {
+          "100%": { transform: "rotate(360deg)" },
+        },
+        dash: {
+          "0%": { transform: "rotate(0deg)", "stroke-dashoffset": 204 },
+          "50%": { transform: "rotate(45deg)", "stroke-dashoffset": 52 },
+          "100%": { transform: "rotate(360deg)", "stroke-dashoffset": 204 },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
 };
