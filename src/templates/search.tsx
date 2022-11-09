43f1910ff33201ae01273431625b54054f128eff
Aaron Pavlick <apavlick@yext.com>
2022-11-08 16:42:03 -0800

    platform config

---
+++ src/templates/search.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import {
+  Template,
+  GetPath,
+  TemplateRenderProps,
+  GetHeadConfig,
+  HeadConfig,
+  TemplateProps,
+} from "@yext/pages";
+import "../index.css";
+
+export const getPath: GetPath<TemplateProps> = () => {
+  return "/search";
+};
+
+export const getHeadConfig: GetHeadConfig<
+  TemplateRenderProps
+> = (): HeadConfig => {
+  return {
+    title: `Turtlehead Tacos Search`,
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+  };
+};
+
+const Search: Template<TemplateRenderProps> = () => {
+  return <>Your Code Here!</>;
+};
+
+export default Search;
